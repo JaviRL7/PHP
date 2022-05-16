6b6fcc446680227fae1fdac2f3ffd71e27dad4be
JaviRL7 <Jrlsanlucar11@gmail.com>
2022-05-16 19:26:38 +0200

    Actividad 6

---
+++ ACT6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 6</title>
+</head>
+<body>
+<h1>Actividad bloque 2</h1>
+<form action="./ACT6v2.php" method="get">
+    <p>Nombre cliente: <input id="nombrecliente" type="text" name="nombre"/></p>
+    <br>
+    <p>Apellido 1 cliente: <input id="apellido1" type="text" name="apellido1"/></p>
+    <br>
+    <p>Apellido 2 cliente: <input id="apellido2" type="text" name="apellido2"/></p>
+    <p><input type="submit" value="Enviar"/></p>
+
+</form> 
+</body>
+</html>
---
+++ ACT6v2.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parte de la conexion con Postgres</title>
+</head>
+<body>
+    <?php
+        $dsn = 'pgsql:dbname=jrodriguez_fact;host=192.168.56.101;port=5432';
+        $user = 'jrodriguez';
+        $password = '1*pharmaton';
+        try{
+            $pdo = new PDO( $dsn, $user, $password);
+            echo 'Conexión Correcta';
+            } 
+            catch( PDOException $e ) {
+            echo 'Error al conectarnos: '.$e->getMessage();
+            }
+        /* Datos del forulario */
+        
+        $nombre = $_REQUEST['nombre'];
+        $apellido1 = $_REQUEST['apellido1'];
+        $apellido2 = $_REQUEST['apellido2'];
+        /*comando*/
+        $query = "SELECT * FROM CLIENTE WHERE nombre = '$nombre' and apellido1= '$apellido1' and apellido2= '$apellido2'";
+        $results = $pdo->query($query);
+        /*Depuracion*/
+        echo "<table border='1'>\n";
+        echo "<tr>\n";
+        echo "<th>Cod Cliente</th>\n";
+        echo "<th>Nombre</th>\n";
+        echo "<th>Apellido 1</th>\n";
+        echo "<th>Apellido 2</th>\n";
+        echo "<th>Credito</th>\n";
+        echo "<th>Ciudad</th>\n";
+        echo "<th>dni</th>\n";
+        echo "</tr>\n";
+        foreach ($results as $row){
+            echo "<tr>\n";
+            echo "<td>".$row['cod_cliente']."</td>";
+            echo "<td>".$row['nombre']."</td>";
+            echo "<td>".$row['apellido1']."</td>";
+            echo "<td>".$row['apellido2']."</td>";
+            echo "<td>".$row['credito']."</td>";
+            echo "<td>".$row['ciudad']."</td>";
+            echo "<td>".$row['dni']."</td>";
+            echo "</tr>";
+        }
+            echo "</table>\n"
+        ?>
+</body>
+</html>
