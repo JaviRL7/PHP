5b4dea534c99ab7fbb5b76eed631c62c0bcf9488
JaviRL7 <Jrlsanlucar11@gmail.com>
2022-05-16 17:46:07 +0200

    Ej bloque 2

---
+++ B2EJ1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 del bloque 2</title>
+</head>
+<body>
+<h1>Ejercicio 1 bloque 2</h1>
+<form action="./EJ3v2.php" method="get">
+    <p>Codigo cliente: <input id="cod_cliente" type="int" name="cod"/></p>
+    <br>
+    <p>Nombre cliente: <input id="nombrecliente" type="text" name="nombre"/></p>
+    <br>
+    <p>Apellido 1 cliente: <input id="apellido1" type="text" name="apellido2"/></p>
+    <br>
+    <p>Apellido 2 cliente: <input id="apellido2" type="text" name="apellido2"/></p>
+    <br>
+    <p>Credito cliente: <input id="credito" type="int" name="credito"/></p>
+    <br>
+    <p>Ciudad cliente: <input id="credito" type="int" name="ciudad"/></p>
+    <br>
+    <p>dni cliente: <input id="dni" type="text" name="dni"/></p>
+    <p><input type="submit" value="Enviar"/></p>
+
+</form> 
+</body>
+</html>
---
+++ B2EJ1v2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parte de la conexion con Postgres</title>
+</head>
+<body>
+    <h1>Conexion con Postgres</h1>
+    <?php
+         $dsn = 'pgsql:dbname=magarciadb;host=localhost;port=5432';
+        ?>
+</body>
+</html>
